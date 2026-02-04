beee5f95e89703a4bf3c52626beba06c2070912e
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-04 17:22:48 +0600

    conception

---
+++ obj.js
@@ -0,0 +1,7 @@
+ 
+ function object(attribute){
+    const count=Object.keys(attribute).length;
+    return count;
+ }
+const objectresult=object({name:'jahid',Last_Name:'Hasan',location:'netrokuna',age:22});
+console.log(objectresult);
