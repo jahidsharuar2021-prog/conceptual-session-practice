5ee9dbd5e5d5d568cb3b920306ec9f3b7210acf1
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-04 18:21:06 +0600

    word count

--- wordcount.js
+++ wordcount.js
@@ -2,19 +2,19 @@ function wordcount(sentence){
     console.log(sentence);
     let nullobj={};
     const singleword=sentence.split(" "); // space conver releted divide kore
-    console.log(singleword);
+    
     for(everyword of singleword){ //string a conver korlam
-        console.log(everyword);
-        if( nullobj .hasOwnProperty(everyword)){
-           nullobj [everyword] ++;
+        let word=everyword.toLowerCase();
+        if( nullobj .hasOwnProperty(word)){
+           nullobj [word] ++;
         }
         else {
-        nullobj [everyword]=1;
+        nullobj [word]=1;
         }
     }
 
  return nullobj;
 
 }
-const result=wordcount("I love js and I love coding and js is fun");
+const result=wordcount("I love js and i love coding and js is fun Fun");
 console.log(result);
